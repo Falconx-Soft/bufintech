278ee350aebfda08cdbacdeb9d10150257fdcea6
Tayyab <F2017065009@umt.edu.pk>
2022-01-19 23:47:38 +0500

    profile and subscription page

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,4 +157,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kabocha608@gmail.com'
-EMAIL_HOST_PASSWORD = 'L00000000'
+EMAIL_HOST_PASSWORD = 'pqzeytgaklzhxzzq'
